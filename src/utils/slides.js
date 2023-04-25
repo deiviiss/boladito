1cf67c8e13b01c57ef6cf060fc393501053a36ed
David Hilera <david.hilera@hotmail.com>
2023-04-24 18:20:54 -0600

    feat: :sparkles: Add Carousel
    Add component carousel - Add styled components - Fix color in navbar and body.

---
+++ src/utils/slides.js
@@ -0,0 +1,14 @@
+export const slides = [
+  {
+    id: 1,
+    url: 'https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    id: 2,
+    url: 'src/assets/slides/slide-demo.png'
+  },
+  {
+    id: 3,
+    url: 'src/assets/slides/slide-demo.png'
+  }
+]
