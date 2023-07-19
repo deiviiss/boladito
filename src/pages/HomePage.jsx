d5f9f7f178a33eca930d9511836db22d6549a759
David Hilera <david.hilera@hotmail.com>
2023-07-19 06:33:39 -0600

    docs: :arrow_up: Update PNPM

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import iconFacebook from '../assets/icons/Facebook.svg'
 import iconInstagram from '../assets/icons/Instagram.svg'
